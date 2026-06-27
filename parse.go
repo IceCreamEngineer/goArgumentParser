ea1bb251d345dc6f6999985fc43a8b3fbc686714
Matt Pistacchio <mpistacchio77@gmail.com>
2026-06-27 18:59:39 -0400

    Early working client

--- parse.go
+++ parse.go
@@ -1,9 +1,12 @@
-package goArgumentParser
+// usr/bin/true; exec /usr/bin/env go run "$0" "$@"
+package main
 
 import (
+	"fmt"
 	"goArgumentParser/adapters"
 	"goArgumentParser/entities"
 	"goArgumentParser/useCases"
+	"os"
 )
 
 func main() {
@@ -13,9 +16,11 @@ func main() {
 	arrayRequired := false
 	schema := []entities.ArgumentSchemaElement{{Name: "s", LongName: "string", ArgumentType: "*", Description: "A string argument."},
 		{Name: "a", LongName: "stringArray", ArgumentType: "[*]", Description: "A string array argument.", Required: &arrayRequired}}
-	argumentParser := useCases.ArgumentParser{Schema: schema, MarshalerFactory: argumentMarshalerFactory, HelpMessagePresenter: helpMessagePresenter}
+	arguments := os.Args[1:]
+	argumentParser := useCases.ArgumentParser{Arguments: arguments, Schema: schema, MarshalerFactory: argumentMarshalerFactory, HelpMessagePresenter: helpMessagePresenter}
 	err := argumentParser.Parse()
 	if err != nil {
 		panic(err)
 	}
+	presenter.Present("My string argument is " + fmt.Sprint(argumentParser.GetValueOf(entities.ArgumentNames{Name: "s", LongName: "string"})))
 }
